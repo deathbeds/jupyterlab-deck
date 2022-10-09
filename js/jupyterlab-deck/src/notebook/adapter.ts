e5b2723fcc0d9f478dd1e8b180137239d52bc834
Nicholas Bollweg <nick.bollweg@gmail.com>
2022-10-09 12:21:33 -0500

    rework notebook extent building

--- js/jupyterlab-deck/src/notebook/adapter.ts
+++ js/jupyterlab-deck/src/notebook/adapter.ts
@@ -72,19 +72,39 @@ export class NotebookAdapter implements IDeckAdapter<NotebookPanel> {
 
   /** move around */
   public go = async (notebook: NotebookPanel, direction: TDirection): Promise<void> => {
-    switch (direction) {
-      case DIRECTION.forward:
-        notebook.content.activeCellIndex += 1;
-        break;
-      case DIRECTION.back:
-        notebook.content.activeCellIndex -= 1;
-        break;
-      case DIRECTION.up:
-      case DIRECTION.down:
-        console.warn(
-          EMOJI + this._manager.__('Going "%1" in Deck not implemented yet', direction)
-        );
-        break;
+    const { activeCellIndex } = notebook.content;
+    const extents = this._getExtents(notebook.content);
+    const activeExtent = extents.get(activeCellIndex);
+
+    const fromExtent = activeExtent && activeExtent[direction];
+
+    if (fromExtent != null) {
+      notebook.content.activeCellIndex = fromExtent;
+    } else {
+      console.warn(
+        EMOJI,
+        this._manager.__('Fallback transition'),
+        activeCellIndex,
+        direction
+      );
+      switch (direction) {
+        case 'up':
+        case 'back':
+          notebook.content.activeCellIndex--;
+          break;
+        case 'forward':
+        case 'down':
+          notebook.content.activeCellIndex++;
+          break;
+        default:
+          console.error(
+            EMOJI,
+            this._manager.__('Unhandled transition'),
+            activeCellIndex,
+            direction
+          );
+          break;
+      }
     }
   };
 
@@ -92,7 +112,7 @@ export class NotebookAdapter implements IDeckAdapter<NotebookPanel> {
     notebook: Notebook,
     cell: Cell<ICellModel>
   ): Promise<void> {
-    const extents = this._getNotebookExtents(notebook);
+    const extents = this._getExtents(notebook);
 
     const { activeCellIndex } = notebook;
     let activeExtent = extents.get(activeCellIndex);
@@ -102,6 +122,12 @@ export class NotebookAdapter implements IDeckAdapter<NotebookPanel> {
       notebook.activeCellIndex = activeCellIndex + offset;
       return;
     }
+    console.warn(
+      this._previousActiveCellIndex,
+      activeCellIndex,
+      activeExtent.onScreen,
+      activeExtent.visible
+    );
 
     this._previousActiveCellIndex = activeCellIndex;
 
@@ -125,80 +151,164 @@ export class NotebookAdapter implements IDeckAdapter<NotebookPanel> {
     return ((cell.model.metadata.get('slideshow') || {}) as any)['slide_type'] || null;
   }
 
-  /** Build a model of what would be on-screen(s) for a given index */
-  protected _getNotebookExtents(notebook: Notebook): NotebookAdapter.TExtentMap {
-    let idx = 0;
-    let extents: NotebookAdapter.TExtentMap = new Map();
-    let currentSlide: NotebookAdapter.IExtent = {
-      onScreen: [],
-      visible: [],
+  protected _startExtent(
+    index: number,
+    slideType: TSlideType,
+    extent: Partial<NotebookAdapter.IExtent> = {}
+  ): NotebookAdapter.IExtent {
+    return {
+      index,
+      slideType,
+      parent: extent.parent ? extent.parent : null,
+      onScreen: extent.onScreen != null ? extent.onScreen : [],
+      visible: extent.visible ? extent.visible : [],
       notes: [],
+      forward: extent.forward != null ? extent.forward : null,
+      back: extent.back != null ? extent.back : null,
+      up: extent.up != null ? extent.up : null,
+      down: extent.down != null ? extent.down : null,
     };
-    let currentExtents: NotebookAdapter.TExtentMap = new Map();
-    let fragmentExtent: NotebookAdapter.IExtent | null = null;
-    let nullExtent: NotebookAdapter.IExtent | null = null;
-    let lastAnnotated = -1;
+  }
+
+  protected _lastOnScreenOf(
+    index: number,
+    extents: NotebookAdapter.TExtentMap
+  ): null | NotebookAdapter.IExtent {
+    let e = extents.get(index);
+    if (!e) {
+      return null;
+    }
+    return extents.get(e.onScreen[0]) || null;
+  }
+
+  /** Build a cell index (not id) map of what would be on-screen(s) for a given index
+   *
+   * gather:
+   * - the index
+   * - what is forward/back/up/down
+   *   - fragments advance on down|forward, reverse on up|back
+   * - what is onscreen
+   * - what is visible
+   * - what are the notes
+   */
+  protected _getExtents(notebook: Notebook): NotebookAdapter.TExtentMap {
+    const extents: NotebookAdapter.TExtentMap = new Map();
+    const stacks: Record<string, NotebookAdapter.IExtent[]> = {
+      slides: [],
+      subslides: [],
+      fragments: [],
+    };
+
+    let index = -1;
     for (const cell of notebook.widgets) {
+      index++;
       let slideType = this._getSlideType(cell);
+      let extent = this._startExtent(index, slideType);
+      let s0: NotebookAdapter.IExtent | null = stacks.slides[0];
+      let ss0: NotebookAdapter.IExtent | null = stacks.subslides[0];
+      let f0: NotebookAdapter.IExtent | null = stacks.fragments[0];
+      let a0 = f0 || ss0 || s0;
+      if (index === 0 && (slideType == null || slideType === 'subslide')) {
+        slideType = 'slide';
+      }
       switch (slideType) {
-        case 'subslide':
-        case 'slide':
-          currentSlide = { onScreen: [idx], visible: [idx], notes: [] };
-          currentExtents = new Map();
-          currentExtents.set(idx, currentSlide);
-          extents.set(idx, currentSlide);
-          lastAnnotated = idx;
-          fragmentExtent = null;
-          nullExtent = null;
-          break;
+        case 'skip':
+          continue;
         case null:
-          for (let otherExtent of currentExtents.values()) {
-            otherExtent.onScreen.push(idx);
+          if (f0) {
+            for (let f of stacks.fragments) {
+              f.onScreen.unshift(index);
+            }
+            f0.visible.unshift(index);
+            f0.forward = f0.down = index;
+            extent.back = extent.up = f0.index;
+          } else if (ss0) {
+            ss0.onScreen.unshift(index);
+            ss0.visible.unshift(index);
+            ss0.forward = index;
+            extent.forward = extent.down = ss0.index;
+          } else if (s0) {
+            s0.onScreen.unshift(index);
+            s0.visible.unshift(index);
+            s0.forward = s0.down = index;
           }
-          if (fragmentExtent) {
-            fragmentExtent.visible.push(idx);
-          } else {
-            currentSlide.visible.push(idx);
+          stacks.fragments.unshift(extent);
+          extent.onScreen.unshift(...(a0?.onScreen || []));
+          extent.visible.unshift(...(a0?.visible || []));
+          break;
+        case 'slide':
+          if (stacks.subslides.length && s0) {
+            // this is not strictly accurate, needs state for `y` in previous subslide stack
+            extent.back = s0.index;
+            for (let s of [...stacks.subslides, s0]) {
+              let lastOnScreen = this._lastOnScreenOf(s.index, extents);
+              if (lastOnScreen) {
+                lastOnScreen.forward = index;
+              }
+            }
+          } else if (f0 && s0) {
+            f0.forward = index;
+            extent.back = f0.index;
+          } else if (s0) {
+            let lastOnScreen = this._lastOnScreenOf(s0.index, extents);
+            if (lastOnScreen) {
+              lastOnScreen.forward = index;
+              extent.back = lastOnScreen.index;
+            }
           }
-          nullExtent = {
-            onScreen: [...currentSlide.onScreen, idx],
-            visible: [
-              ...currentSlide.visible,
-              ...((nullExtent && nullExtent.visible) || []),
-              ...((fragmentExtent && fragmentExtent.visible) || []),
-              idx,
-            ],
-            notes: [],
-          };
-          currentExtents.set(idx, nullExtent);
-          extents.set(idx, nullExtent);
-          lastAnnotated = idx;
+          stacks.fragments = [];
+          stacks.subslides = [];
+          stacks.slides.unshift(extent);
+          extent.onScreen.unshift(index);
+          extent.visible.unshift(index);
+          break;
+        case 'subslide':
+          if (f0) {
+            f0.down = index;
+            extent.up = f0.index;
+          } else if (s0) {
+            s0.down = index;
+            extent.up = s0.index;
+            if (s0.back != null) {
+              extent.back = s0.back;
+            }
+          }
+          stacks.fragments = [];
+          stacks.subslides.unshift(extent);
+          extent.onScreen.unshift(index);
+          extent.visible.unshift(index);
           break;
         case 'fragment':
-          for (let otherExtent of currentExtents.values()) {
-            otherExtent.onScreen.push(idx);
+          if (f0) {
+            f0.down = f0.forward = index;
+            extent.back = f0.index;
+            for (let f of [...stacks.fragments, ss0 || s0]) {
+              f.onScreen.unshift(index);
+            }
+          } else if (ss0) {
+            ss0.onScreen.unshift(index);
+            ss0.down = ss0.forward = index;
+            extent.up = extent.back = ss0.index;
+          } else if (s0) {
+            s0.onScreen.unshift(index);
+            s0.down = s0.forward = index;
+            extent.up = extent.back = s0.index;
           }
-          fragmentExtent = {
-            onScreen: [...currentSlide.onScreen, idx],
-            visible: [
-              ...currentSlide.visible,
-              ...((fragmentExtent && fragmentExtent.visible) || []),
-              idx,
-            ],
-            notes: [],
-          };
-          currentExtents.set(idx, fragmentExtent);
-          extents.set(idx, fragmentExtent);
+          extent.onScreen.unshift(...(a0?.onScreen || []));
+          extent.visible.unshift(index, ...(a0?.visible || []));
+          stacks.fragments.unshift(extent);
           break;
         case 'notes':
-          if (lastAnnotated !== -1) {
-            extents.get(lastAnnotated)?.notes.push(idx);
+          if (f0) {
+            f0.notes.push(index);
+          } else if (ss0) {
+            ss0.notes.push(index);
+          } else if (s0) {
+            s0.notes.push(index);
           }
           break;
-        case 'skip':
-          break;
       }
-      idx += 1;
+      extents.set(index, extent);
     }
     return extents;
   }
@@ -210,6 +320,13 @@ export namespace NotebookAdapter {
     commands: CommandRegistry;
   }
   export interface IExtent {
+    slideType: TSlideType;
+    parent: IExtent | null;
+    index: number;
+    up: number | null;
+    down: number | null;
+    forward: number | null;
+    back: number | null;
     onScreen: number[];
     visible: number[];
     notes: number[];
